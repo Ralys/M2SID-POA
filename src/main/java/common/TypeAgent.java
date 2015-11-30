cb364745e3382269cb179d17492024a7b729a196
Ralys <ralys@gmx.fr>
2015-11-30 15:47:53 +0100

    Ajout des classes communes à toutes les groupes

---
+++ src/main/java/common/TypeAgent.java
@@ -0,0 +1,12 @@
+package common;
+
+/**
+ * Les 4 différents types d'Agent qu'on peut retrouver
+ * @author Mazzei Stéphane
+ */
+public final class TypeAgent {
+    public static final String Client = "Client";
+    public static final String EReputation = "EReputation";
+    public static final String Fournisseur = "Fournisseur";
+    public static final String Vendeur = "Vendeur";
+}

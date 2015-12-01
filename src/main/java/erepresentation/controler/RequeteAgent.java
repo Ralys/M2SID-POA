e4af46e6170e285a341f1474e64d00c0ba33120c
gregoireMIAGE <gregoire57710@hotmail.fr>
2015-12-01 18:20:55 +0100

    creation d'une classe requête agent. Celui permet de définir chaque fonction demandeAvisFourniseur, demandeAvisVendeur, demandeAvisProduit, donneAvisVendeur, donneAvisFournisseur, donneAvisProduit

---
+++ src/main/java/erepresentation/controler/RequeteAgent.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package erepresentation.controler;
+
+/**
+ *
+ * @author greg
+ */
+public class RequeteAgent {
+    
+}

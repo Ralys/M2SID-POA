993b703b51746cc37b90ae2c87ae66f3472616f6
tom <tom@tom>
2015-12-02 17:50:40 +0100

    strategie fournisseur :  définir prix basique

--- src/main/java/fournisseur/behaviors/WaitRequestStrategie1.java
+++ src/main/java/fournisseur/behaviors/WaitRequestStrategie1.java
@@ -8,13 +8,13 @@ import fournisseur.Stocks;
  * @author Tom
  */
 public class WaitRequestStrategie1 extends WaitRequest{
-    private double margeMin = 1.10;
+    private double margeBase = 1.10;
     private double reductionQte = 0.01;
     
     @Override
     public double definirPrix(int idProduit, int quantite, int delai) {
         double prixBase = ((Stocks)getDataStore()).getProduitById(idProduit).getPrixdeBase();
-        return ((prixBase * margeMin) * (1-(reductionQte*quantite)))+Livraison.prixLivraisonByDelai(delai);
+        return ((prixBase * margeBase) * (1-(reductionQte*quantite)))+Livraison.prixLivraisonByDelai(delai);
     }
     
 }

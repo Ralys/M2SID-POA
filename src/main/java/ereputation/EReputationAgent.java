eb993598655ec1781a1f5aaddc2e9d95455e0a69
Ralys <ralys@gmx.fr>
2015-12-03 00:02:07 +0100

    Ajout de la méthode tearDown dans EReputationAgent

--- src/main/java/ereputation/EReputationAgent.java
+++ src/main/java/ereputation/EReputationAgent.java
@@ -5,8 +5,12 @@ import common.TypeAgent;
 import ereputation.behaviours.InformBehaviour;
 import ereputation.behaviours.RequestBehaviour;
 import jade.core.AID;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -27,6 +31,15 @@ public class EReputationAgent extends SuperAgent {
        
         this.addBehaviour(new RequestBehaviour(this));
         this.addBehaviour(new InformBehaviour(this));
+    }
+    
+    @Override
+    protected void takeDown() {
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException ex) {
+            Logger.getLogger(EReputationAgent.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     public AID getBDDAgent() {

ff189a541dcabf4c79a99cfbf23a368ab46859f3
tom <tom@tom>
2015-12-02 15:43:19 +0100

    Ajout prix de la livraison selon délai

--- src/main/java/fournisseur/Livraison.java
+++ src/main/java/fournisseur/Livraison.java
@@ -8,20 +8,29 @@ import java.util.HashMap;
  *
  * @author tom
  */
-public class DateLivraison {
+public class Livraison {
+    
+    private static HashMap<Integer,Double> prixLivraison;
+    
+    static{
+        prixLivraison = new HashMap<>();
+        prixLivraison.put(10, 0.0);
+        prixLivraison.put(11, 10.0);
+        prixLivraison.put(3, 5.0);
+    }
 
     public static HashMap<Integer, Date> getListeDateLivraison() {
         Date dateJour = new Date();
         HashMap<Integer, Date> listDate = new HashMap<>();
 
         //Livraison express : 1 jour
-        listDate.put(1, DateLivraison.addDays(dateJour, 1));
+        listDate.put(1, Livraison.addDays(dateJour, 1));
 
         //Livraison normale : 3 jours
-        listDate.put(3, DateLivraison.addDays(dateJour, 3));
+        listDate.put(3, Livraison.addDays(dateJour, 3));
 
         //Livraison la poste : 10 jours
-        listDate.put(10, DateLivraison.addDays(dateJour, 10));
+        listDate.put(10, Livraison.addDays(dateJour, 10));
 
         return listDate;
     }
@@ -31,6 +40,10 @@ public class DateLivraison {
         cal.setTime(date);
         cal.add(Calendar.DATE, days);
         return cal.getTime();
+    }
+    
+    public static double prixLivraisonByDelai(int delai){
+        return prixLivraison.get(delai);
     }
     
     

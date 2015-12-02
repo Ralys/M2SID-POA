ff72953c73fce12fae6892a7b5a4532dd766cdb7
Ralys <ralys@gmx.fr>
2015-12-03 00:24:35 +0100

    Ajout de la méthode commune findAgentsFromService(String)

--- src/main/java/common/SuperAgent.java
+++ src/main/java/common/SuperAgent.java
@@ -1,8 +1,10 @@
 package common;
 
+import jade.core.AID;
 import jade.core.Agent;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.SearchConstraints;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
 
@@ -32,5 +34,31 @@ public class SuperAgent extends Agent {
             System.err.println(getLocalName() + " registration with DF unsucceeded. Reason: " + e.getMessage());
             doDelete();
         }
+    }
+    
+    protected AID[] findAgentsFromService(String service) {
+        DFAgentDescription dfd = new DFAgentDescription();
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType(service);
+        dfd.addServices(sd);
+
+        SearchConstraints ALL = new SearchConstraints();
+        ALL.setMaxResults(new Long(-1));
+
+        AID[] agents = null;
+        
+        try {
+            DFAgentDescription[] result = DFService.search(this, dfd, ALL);
+            agents = new AID[result.length];
+            
+            for (int i = 0; i < result.length; i++) {
+                agents[i] = result[i].getName();
+            }
+            
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+
+        return agents;
     }
 }

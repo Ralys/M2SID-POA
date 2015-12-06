94acd09140c912caf7d1b6f1cad9b46fbc57b6bc
mercier-remi <mercier1193@gmail.com>
2015-12-06 14:06:56 +0100

    Javadoc Client FXMLController

--- src/main/java/client/UI/FXMLController.java
+++ src/main/java/client/UI/FXMLController.java
@@ -10,10 +10,6 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.Date;
-import javafx.beans.InvalidationListener;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableBooleanValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -25,82 +21,150 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import common.TypeAgent;
 import common.TypeProduit;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Observable;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.scene.control.DatePicker;
 import javafx.scene.control.RadioButton;
 
+/**
+ * Controleur de l'interface graphique 
+ * @author mercier
+ */
 public class FXMLController implements Initializable {
 
+    /**
+     * Label du message d'erreur lorsque tous les champs ne sont pas remplis
+     */
     @FXML
     private Label messageErreur;
 
+    /**
+     * Champ de saisi de l'IP
+     */
     @FXML
     private TextField ip;
 
+    /**
+     * Champ de saisi du port
+     */
     @FXML
     private TextField port;
 
+    /**
+     * Champ de saisi du nom de l'agent
+     */
     @FXML
     private TextField nomAgent;
 
+    /**
+     * Liste des choix des produits
+     */
     @FXML
     private ComboBox<String> choixProd;
 
+    /**
+     * Choix du type de client
+     */
     @FXML
     private ComboBox<String> choixClient;
 
+    /**
+     * Choix du type de vendeur (fournisseur ou vendeur)
+     */
     @FXML
     private ComboBox<String> choixVendeur;
 
+    /**
+     * Choix de la quantité de 1 à 10
+     */
     @FXML
     private ComboBox<String> choixQte;
 
+    /**
+     * Nom du produit
+     */
     @FXML
     private TextField nomProd;
 
+    /**
+     * Bouton valider permettant de lancer la recherche de produit et d'achat avec la configuration saisi
+     */
     @FXML
     private Button btnValider;
 
+    /**
+     * Quantité du produit
+     */
     @FXML
     private TextField qteProd;
 
+    /**
+     * Panneau contenant le choix du client
+     */
     @FXML
     private AnchorPane selecCli;
 
+    /**
+     * Champ de saisi de la référence du produit à acheter
+     */
     @FXML
     private TextField reference;
 
+    /**
+     * Permet de choisir la recherche par nom de produit
+     */
     @FXML
     private RadioButton rbRecherche;
 
+    /**
+     * Permet de choisir la recherche par ID produit
+     */
     @FXML
     private RadioButton rbReference;
 
+    /**
+     * Permet d'afficher la liste des messages
+     */
     @FXML
     private ListView log;
     
+    /**
+     * Défini la limite
+     */
     @FXML
     private TextField limite;
 
-
+    /**
+     * Initialise la liste des agents vendeur (fournisseur et vendeur)
+     */
     private final ObservableList<String> listTypeAgentVend = FXCollections.observableArrayList(TypeAgent.Fournisseur, TypeAgent.Vendeur);
 
+    /**
+     * Initialise la comboBox des choix de quantité désirée
+     */
     private final ObservableList<String> listQte = FXCollections.observableArrayList(
             "1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
 
+    /**
+     * Initialise la liste des produits disponibles
+     */
     private final ObservableList<String> listTypeProduit = FXCollections.observableArrayList(TypeProduit.allProducts);
 
+    
+    /**
+     * Initialise la liste des différents types de client (comportements du client)
+     */
     private final ObservableList<String> listTypeClient = FXCollections.observableArrayList(TypeAgentClient.Presse, TypeAgentClient.Econome, TypeAgentClient.Mefiant);
 
+    /**
+     * Contient la liste des logs de l'application
+     */
     public static ObservableList<String> listLog = FXCollections.observableArrayList();
 
+    /**
+     * Méthode appelé lors de la création de l'interface javafx
+     * @param url L'URL
+     * @param rb La ressourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         choixProd.setItems(listTypeProduit);
@@ -115,6 +179,10 @@ public class FXMLController implements Initializable {
         nomAgent.setText("Bob");
     }
 
+    /**
+     * Méthode appelé lors de l'appuie sur le bouton valider, permet aussi la créer l'agent cliendt
+     * @param event L'évenement sur le bouton
+     */
     @FXML
     private void handlebtProd(ActionEvent event) {
         try {
@@ -144,6 +212,10 @@ public class FXMLController implements Initializable {
         }
     }
 
+    /**
+     * Permet de désactiver le choix par référence si le choix par nom a été choisi
+     * @param event L'évènement sur le radiobouton
+     */
     @FXML
     private void handleTypeRechercheClassique(ActionEvent event) {
         reference.setDisable(true);
@@ -152,6 +224,10 @@ public class FXMLController implements Initializable {
 
     }
 
+    /**
+     * Permet de désactiver le choix par nom du produit si le choix par référence a été choisi
+     * @param event L'évènement sur le radiobouton
+     */
     @FXML
     private void handleTypeRechercheReference(ActionEvent event) {
         nomProd.setDisable(true);
@@ -159,6 +235,10 @@ public class FXMLController implements Initializable {
         reference.setDisable(false);
     }
 
+    /**
+     * Permet de vérifier si tous les champs ont été remplis
+     * @return true si tous les champs sont remplis, sinon false et affiche un message d'erreur dans les logs
+     */
     public boolean valider() {
         boolean valide = false;
 
@@ -177,10 +257,7 @@ public class FXMLController implements Initializable {
                 && testerLimite(limite)) {
             valide = true;
             messageErreur.setVisible(false);
-
-        }
-        else {
-            /**/
+        } else {
             testerContenu(choixClient, choixVendeur, choixQte);
             testerContenu(ip, port, nomAgent, limite);
 
@@ -192,13 +269,18 @@ public class FXMLController implements Initializable {
                 testerContenu(reference);
             }
             testerLimite(limite);
-           // messageErreur.setText("Merci de remplir tous les champs !");
+            // messageErreur.setText("Merci de remplir tous les champs !");
             //messageErreur.setVisible(true);
         }
 
         return valide;
     }
     
+    /**
+     * Permet de vérifier si la limite saisi est un entier
+     * @param tf Le champ "limite" à vérifier 
+     * @return true si la limite est un entier, sinon false
+     */
     public boolean testerLimite(TextField tf){
         boolean res = false;
         if(!limite.getText().isEmpty()){
@@ -212,12 +294,14 @@ public class FXMLController implements Initializable {
                 limite.setStyle("-fx-border-color:red");
                 res = false;
             }
-               
         }
         return res;
-            
     }
 
+    /**
+     * Permet de vérifié si un choix à bien été fait dans une combobox
+     * @param cbx La comboBox à vérifier. Un message d'erreur dans les logs est affiché.
+     */
     public void testerContenu(ComboBox... cbx) {
         for (ComboBox cb : cbx) {
             if (cb.getSelectionModel().getSelectedIndex() != -1) {
@@ -230,6 +314,10 @@ public class FXMLController implements Initializable {
         }
     }
 
+    /**
+     * Permet de tester si un textField a bien été rempli
+     * @param tfs Le textField à tester. Affiche un message d'erreur dans les logs si le champ n'est pas rempli
+     */
     public void testerContenu(TextField... tfs) {
         for (TextField tf : tfs) {
             if (tf.isVisible() && tf.getText().isEmpty()) {
@@ -238,11 +326,13 @@ public class FXMLController implements Initializable {
                 messageErreur.setVisible(true);
             } else {
                 tf.setStyle("-fx-border-color:green");
-
             }
         }
     }
     
+    /**
+     * Permet d'afficher le champ limite si le client est de type économe ou pressé;
+     */
     public void selectionType(){
         if (choixClient.getSelectionModel().getSelectedItem().equals("Econome")){
             limite.setPromptText("Prix maximal");
